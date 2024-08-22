006fa12d3ae70676d41bfc6013dd10eeb82c318f
wahyupriapurnama <wahyupriapurnama@gmail.com>
2024-08-22 21:06:31 +0700

    oyi

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Selamat Datang di Sistem Informasi AMS</div>
+                    <div class="card-header">Selamat Datang di AMS Information System</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
