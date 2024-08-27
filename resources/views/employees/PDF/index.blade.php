a2431dd451b71fc06f502a79a6286182e836f9cd
wahyupriapurnama <wahyupriapurnama@gmail.com>
2024-08-27 15:52:31 +0700

    pdf exprot

--- resources/views/employees/PDF/index.blade.php
+++ resources/views/employees/PDF/index.blade.php
@@ -6,7 +6,7 @@
         td,
         {
         border: 1px solid black;
-        font-size: 80%;
+        font-size: 50%;
         border-collapse: collapse;
         }
 
@@ -34,12 +34,38 @@
 
     <table>
         <tr>
+            <th colspan="11">ORGANISASI</th>
+            <th colspan="13">BIODATA</th>
+            <th colspan="3">KONTAK DARURAT</th>
+        </tr>
+        <tr>
             <th>NO</th>
             <th>NIP</th>
             <th>NAMA</th>
+            <th>NIK</th>
             <th>PERUSAHAAN</th>
+            <th>DIVISI</th>
+            <th>DEPARTEMEN</th>
+            <th>SEKSI</th>
+            <th>JABATAN</th>
             <th>STATUS</th>
-            <th>HARI</th>
+            <th>-</th>
+            <th>TMPT LHR</th>
+            <th>TGL LHR</th>
+            <th>L/P</th>
+            <th>ALAMAT</th>
+            <th>NO. TELP</th>
+            <th>EMAIL</th>
+            <th>PENDIDIKAN</th>
+            <th>JURUSAN</th>
+            <th>THN LULUS</th>
+            <th>NAMA IBU</th>
+            <th>NPWP</th>
+            <th>STATUS</th>
+            <th>ANAK</th>
+            <th>NAMA</th>
+            <th>NO. TELP</th>
+            <th>HUBUNGAN</th>
         </tr>
 
         {{ $i = 1 }}
@@ -52,14 +78,34 @@
             <td>{{ $i++ }}</td>
             <td> {{ $employee->nip }}</td>
             <td> {{ $employee->nama }}</td>
-
+            <td>{{ $employee->nik }}</td>
             <td>{{ $employee->subsidiary->name }}</td>
+            <td>{{$employee->divisi}}</td>
+            <td>{{$employee->departemen}}</td>
+            <td>{{ $employee->seksi }}</td>
+            <td>{{ $employee->posisi }}</td>
             <td>{{ $employee->status_peg }}</td>
             @if ($employee->status_peg == 'PKWT')
                 <td>{{ Carbon\Carbon::now()->diffInDays($employee->akhir_kontrak) }}</td>
             @else
                 <td> - </td>
             @endif
+            <td>{{$employee->tmpt_lahir}}</td>
+            <td>{{ $employee->tgl_lahir }}</td>
+            <td>{{$employee->jenis_kelamin}}</td>
+            <td>{{$employee->alamat}}</td>
+            <td>{{$employee->no_telp}}</td>
+            <td>{{$employee->email}}</td>
+            <td>{{$employee->pend_trkhr}}</td>
+            <td>{{$employee->jurusan}}</td>
+            <td>{{$employee->thn_lulus}}</td>
+            <td>{{$employee->nama_ibu}}</td>
+            <td>{{$employee->npwp}}</td>
+            <td>{{$employee->status}}</td>
+            <td>{{$employee->jml_ank}}</td>
+            <td>{{$employee->nama_kd}}</td>
+            <td>{{$employee->no_kd}}</td>
+            <td>{{$employee->hubungan}}</td>
             </tr>
         @empty
             <td colspan="6" class="text-center">Tidak ada data...</td>
