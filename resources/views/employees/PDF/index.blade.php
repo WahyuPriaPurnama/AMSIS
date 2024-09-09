4e37ba540d382a11fd838a5597a4b9b3f5b30b97
wahyupriapurnama <wahyupriapurnama@gmail.com>
2024-09-09 10:56:07 +0700

    ok

--- resources/views/employees/PDF/index.blade.php
+++ resources/views/employees/PDF/index.blade.php
@@ -36,7 +36,7 @@
         <tr>
             <th colspan="11">ORGANISASI</th>
             <th colspan="13">BIODATA</th>
-            <th colspan="3">KONTAK DARURAT</th>
+            <th colspan="4">KONTAK DARURAT</th>
         </tr>
         <tr>
             <th>NO</th>
