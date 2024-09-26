ad0396793c58605e926090d8e935a7914b8423cd
wahyupriapurnama <wahyupriapurnama@gmail.com>
2024-09-26 10:23:14 +0700

    ok

--- database/migrations/2024_07_04_042511_create_subsidiaries_table.php
+++ database/migrations/2024_07_04_042511_create_subsidiaries_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('phone');
             $table->text('address');
+            $table->string('logo');
             $table->timestamps();
         });
     }
