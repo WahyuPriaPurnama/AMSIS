047415a4a655237da8d875802bcc40711f40bbd6
wahyupriapurnama <wahyupriapurnama@gmail.com>
2024-10-30 10:59:28 +0700

    ok

--- resources/views/spareparts/index.blade.php
+++ resources/views/spareparts/index.blade.php
@@ -100,7 +100,10 @@
                                     @can('update', App\Models\Sparepart::class)
                                         <button type="button" class="btn btn-success" data-bs-toggle="modal"
                                             data-bs-target="#updateSparepart{{ $item->id }}">
-                                            Update
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exposure" viewBox="0 0 16 16">
+                                                <path d="M8.5 4a.5.5 0 0 0-1 0v2h-2a.5.5 0 0 0 0 1h2v2a.5.5 0 0 0 1 0V7h2a.5.5 0 0 0 0-1h-2zm-3 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1z"/>
+                                                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0M1 8a7 7 0 1 1 14 0A7 7 0 0 1 1 8"/>
+                                              </svg>
                                         </button>
                                     @endcan
                                     <div class="modal fade" id="updateSparepart{{ $item->id }}" data-bs-backdrop="static"
@@ -127,7 +130,11 @@
                                     @can('delete', App\Models\Sparepart::class)
                                         <button type="button" class="btn btn-danger" data-bs-toggle="modal"
                                             data-bs-target="#deleteSparepart{{ $item->id }}">
-                                            Hapus
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
+                                            </svg>
                                         </button>
                                     @endcan
                                     <div class="modal fade" id="deleteSparepart{{ $item->id }}" data-bs-backdrop="static"
