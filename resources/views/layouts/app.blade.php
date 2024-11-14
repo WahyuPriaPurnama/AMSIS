1fe290edcf0caa342a17602d7993966451e9956b
wahyupriapurnama <wahyupriapurnama@gmail.com>
2024-11-14 13:12:24 +0700

    floating button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,39 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
+    <!-- load font awesome for icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
 
+    <!-- add your custom CSS -->
+    <style>
+
+
+        /* Add WA floating button CSS */
+        .floating {
+            position: fixed;
+            width: 60px;
+            height: 60px;
+            bottom: 40px;
+            right: 40px;
+            background-color: #0fe45d;
+            color: #fff;
+            border-radius: 50px;
+            text-align: center;
+            font-size: 30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index: 100;
+        }
+
+        .fab-icon {
+            margin-top: 16px;
+        }
+    </style>
+
+    <!-- render the button and direct it to wa.me -->
+    <a href="https://wa.me/6285745334330?text=Halo," class="floating" target="_blank" >
+        <i class="fab fa-whatsapp fab-icon"></i>
+    </a>
+   
 </head>
 
 <body class="d-flex flex-column min-vh-100">
@@ -99,6 +131,7 @@
                                 </li>
                             @endif
                         @else
+        
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                     data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
