136172cc807c311b7461ca0463b316e3ac1a8b9e
wahyupriapurnama <wahyupriapurnama@gmail.com>
2025-04-23 15:54:50 +0700

    cctv

--- resources/views/cctv.blade.php
+++ resources/views/cctv.blade.php
@@ -7,8 +7,8 @@
             @slot('header')
                 BOFI Live Streaming
             @endslot
-            <div class="text-center">
-                <iframe width='640' height='480' src='https://rtsp.me/embed/5NK4Ysi7/' frameborder='0' allowfullscreen></iframe>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/5NK4Ysi7/' frameborder='0' allowfullscreen></iframe>
             </div>
         @endcomponent
     </div>
