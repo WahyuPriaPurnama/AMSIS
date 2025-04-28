64f5581f7716d3b1dc1b61d6bd961e5de3239886
wahyupriapurnama <wahyupriapurnama@gmail.com>
2025-04-28 13:59:59 +0700

    cctv

--- resources/views/cctv.blade.php
+++ resources/views/cctv.blade.php
@@ -7,11 +7,14 @@
             @slot('header')
                 BOFI Live Streaming
             @endslot
-            <div class="embed-responsive embed-responsive-16by9">
-                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/5NK4Ysi7/' frameborder='0' allowfullscreen></iframe>
-            </div>
-            <div class="embed-responsive embed-responsive-16by9">
+            
+            <div class="embed-responsive embed-responsive-16by9 text-center">
                 <iframe class="embed-responsive-item" src='https://rtsp.me/embed/kBGhz7Ai/"' frameborder='0' allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/a9i7zSzf/' frameborder='0' allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/TGEt73fD/' frameborder='0' allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/5NK4Ysi7/' frameborder='0' allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/byEdirBR/' frameborder='0' allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/YGNZQife/' frameborder='0' allowfullscreen></iframe>
             </div>
         @endcomponent
     </div>
