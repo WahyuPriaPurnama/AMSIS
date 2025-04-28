a2d955e3fd7ba6684e4f24ab5e397d45047d7e2c
wahyupriapurnama <wahyupriapurnama@gmail.com>
2025-04-28 13:22:02 +0700

    cctv gate

--- resources/views/cctv.blade.php
+++ resources/views/cctv.blade.php
@@ -10,6 +10,9 @@
             <div class="embed-responsive embed-responsive-16by9">
                 <iframe class="embed-responsive-item" src='https://rtsp.me/embed/5NK4Ysi7/' frameborder='0' allowfullscreen></iframe>
             </div>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/kBGhz7Ai/"' frameborder='0' allowfullscreen></iframe>
+            </div>
         @endcomponent
     </div>
 
