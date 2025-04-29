8885c7d74c0771575cef4218f08715ff23170ae5
wahyupriapurnama <wahyupriapurnama@gmail.com>
2025-04-29 08:19:33 +0700

    cctv bofi

--- resources/views/cctv.blade.php
+++ resources/views/cctv.blade.php
@@ -7,14 +7,26 @@
             @slot('header')
                 BOFI Live Streaming
             @endslot
-            
+
             <div class="embed-responsive embed-responsive-16by9 text-center">
-                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/kBGhz7Ai/"' frameborder='0' allowfullscreen></iframe>
-                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/a9i7zSzf/' frameborder='0' allowfullscreen></iframe>
-                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/TGEt73fD/' frameborder='0' allowfullscreen></iframe>
-                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/5NK4Ysi7/' frameborder='0' allowfullscreen></iframe>
-                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/byEdirBR/' frameborder='0' allowfullscreen></iframe>
-                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/YGNZQife/' frameborder='0' allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/kBGhz7Ai/"' frameborder='0'
+                    allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/a9i7zSzf/' frameborder='0'
+                    allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/TGEt73fD/' frameborder='0'
+                    allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/5NK4Ysi7/' frameborder='0'
+                    allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/byEdirBR/' frameborder='0'
+                    allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/YGNZQife/' frameborder='0'
+                    allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/3n7sbrrE/' frameborder='0'
+                    allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/BnHYBn6F/' frameborder='0'
+                    allowfullscreen></iframe>
+                <iframe class="embed-responsive-item" src='https://rtsp.me/embed/NbS47S7k/' frameborder='0'
+                    allowfullscreen></iframe>
             </div>
         @endcomponent
     </div>
