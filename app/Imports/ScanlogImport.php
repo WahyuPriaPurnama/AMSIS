63b69a83e7ec8aa88b199be134ab194ba604e2a9
wahyupriapurnama <wahyupriapurnama@gmail.com>
2025-05-09 13:38:41 +0700

    ok

--- app/Imports/ScanlogImport.php
+++ app/Imports/ScanlogImport.php
@@ -14,7 +14,9 @@ class ScanlogImport implements ToModel
      */
     public function model(array $row)
     {
-
+        if ($row == '') {
+            $row = null;
+        }
         return new Scanlog([
             'pin' => $row[0],
             'nip' => $row[1],
@@ -23,10 +25,10 @@ class ScanlogImport implements ToModel
             'bagian' => $row[4],
             'upah' => $row[5],
             'tanggal' => date('Y-m-d', strtotime($row[6])),
-            'scan_1' => $row[7] ? '' : null,
-            'scan_2' => $row[8] ? '' : null,
-            'scan_3' => $row[9] ? '' : null,
-            'scan_4' => $row[10] ? '' : null,
+            'scan_1' => $row[7],
+            'scan_2' => $row[8],
+            'scan_3' => $row[9],
+            'scan_4' => $row[10],
         ]);
     }
 }
