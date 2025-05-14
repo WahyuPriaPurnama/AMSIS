782ed921a5700e1cf31b8b08032ed5a921a93a2a
wahyupriapurnama <wahyupriapurnama@gmail.com>
2025-05-14 11:33:35 +0700

    cleaning

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,9 +20,9 @@
     <!-- load font awesome for icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
 
-   <!-- DataTables CSS -->
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
 
 
     <!-- add your custom CSS -->
@@ -137,7 +137,7 @@
                                     @endcan
                                 </ul>
                             </li>
-                          
+
                             <li class="nav-item">
                                 <a href="/cctv" class="nav-link @yield('menuCCTV')">
                                     CCTV
