31b0f2ac3e2ffbd891d4b185418f64cae082edc9
wahyupriapurnama <wahyupriapurnama@gmail.com>
2025-06-02 15:02:05 +0700

    remove tombol edit jam

--- resources/views/scanlog/index.blade.php
+++ resources/views/scanlog/index.blade.php
@@ -8,14 +8,14 @@
                 <button type="button" class="btn btn-success w-auto h-100" data-bs-toggle="modal" data-bs-target="#import">
                     <i class="bi bi-file-earmark-arrow-down-fill">
                     </i>
-                    <i class="bi bi-file-earmark-spreadsheet-fill"></i>
+                    Import
                 </button>
-                <a href="{{ route('scanlog.export') }}" class="btn btn-success w-auto h-100"><i
-                        class="bi bi-file-earmark-arrow-up-fill"></i>
-                    <i class="bi bi-file-earmark-spreadsheet-fill"></i></a>
-                <button type="button" class="btn btn-warning w-auto h-100" data-bs-toggle="modal" data-bs-target="#ctime">
+                <a href="{{ route('scanlog.export') }}" class="btn btn-success w-auto h-100">
+                    <i class="bi bi-file-earmark-arrow-up-fill"></i>
+                    Export</a>
+                {{-- <button type="button" class="btn btn-warning w-auto h-100" data-bs-toggle="modal" data-bs-target="#ctime">
                     <i class="bi bi-pencil-square"></i>
-                </button>
+                </button> --}}
                 <button type="submit" class="btn btn-success w-auto h-100" form="myForm"><i class="bi bi-clock-fill"></i>
                     Konversikan</button>
                 <a href="{{ route('scanlog.truncate') }}" class="btn btn-secondary w-auto h-100" data-toggle="tooltip"
@@ -25,10 +25,11 @@
                     @csrf
                     <input type="hidden" name="jam" value="60">
                 </form>
-                {{-- <div class="alert alert-warning ms-auto">
+            </div>
+            {{-- <div class="alert alert-warning ms-auto">
                     <i class="bi bi-info-circle-fill"></i> Durasi Kerja >= 10 jam potongan istirahat 1,5 jam
                 </div> --}}
-            </div>
+       
         @slot('header')
             Data Scanlog
         @endslot
@@ -81,74 +82,74 @@
                             </span>
                         @endif
                     </td>
-                    </tr>
+                   
                     @endforeach
                 </tbody>
             </table>
-        </div>
-    @endcomponent
-    </div>
-    <div class="modal fade" id="import" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">IMPORT DATA</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                    </button>
-                </div>
-                <form action="{{ route('scanlog.import') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <label>PILIH FILE</label>
-                            <input type="file" name="file" class="form-control @error('file') is-invalid @enderror"
-                                required>
-                            @error('file')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
+        @endcomponent
+
+        <div class="modal fade" id="import" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">IMPORT DATA</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                        </button>
+                    </div>
+                    <form action="{{ route('scanlog.import') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label>PILIH FILE</label>
+                                <input type="file" name="file"
+                                    class="form-control @error('file') is-invalid @enderror" required>
+                                @error('file')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
+                            </div>
                         </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">TUTUP</button>
-                        <button type="submit" class="btn btn-success">IMPORT</button>
-                    </div>
-                </form>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">TUTUP</button>
+                            <button type="submit" class="btn btn-success">IMPORT</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="modal fade" id="ctime" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Pilih Range Jam</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                    </button>
+        <div class="modal fade" id="ctime" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Pilih Range Jam</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                        </button>
+                    </div>
+                    <form action="{{ route('scanlog.ctime') }}" method="post">
+                        @csrf
+                        <div class="modal-body">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <label for="">Range Bawah</label>
+                                    <input type="time" name="awal" id="" class="form-control">
+                                </div>
+                                <div class="col">
+                                    <label for="">Range Atas</label>
+                                    <input type="time" name="akhir" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                    <label for="">Menjadi</label>
+                                    <input type="time" name="waktu" class="form-control" id="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">TUTUP</button>
+                            <button type="submit" class="btn btn-success">PROSES</button>
+                        </div>
+                    </form>
                 </div>
-                <form action="{{ route('scanlog.ctime') }}" method="post">
-                    @csrf
-                    <div class="modal-body">
-                        <div class="row mb-3">
-                            <div class="col">
-                                <label for="">Range Bawah</label>
-                                <input type="time" name="awal" id="" class="form-control">
-                            </div>
-                            <div class="col">
-                                <label for="">Range Atas</label>
-                                <input type="time" name="akhir" class="form-control">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col">
-                                <label for="">Menjadi</label>
-                                <input type="time" name="waktu" class="form-control" id="">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">TUTUP</button>
-                        <button type="submit" class="btn btn-success">PROSES</button>
-                    </div>
-                </form>
             </div>
         </div>
     </div>
