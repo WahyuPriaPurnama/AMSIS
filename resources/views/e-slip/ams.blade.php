5705c041537013e58d8c61ec9580133d1d137244
wahyupriapurnama <wahyupriapurnama@gmail.com>
2025-06-03 14:11:30 +0700

    hapus baris spasi

--- resources/views/e-slip/ams.blade.php
+++ resources/views/e-slip/ams.blade.php
@@ -11,14 +11,12 @@
                 <table class="table table-hover display" id="table">
                     <thead>
                         <tr>
-
                             <th>NAMA</th>
                             <th>E-SLIP</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-
                             <td>A Lofie Vila Sarie</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/mjxpp0J94"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -28,7 +26,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Abbiyu Laily Putri</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/X9G1ia030"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -38,7 +35,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Ade Ayu Santi</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/MkNcD9EgW"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -48,7 +44,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Alifyya Faiza</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/InnFtW30J"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -58,7 +53,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Denny Setyawan</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/Nl8gUH80R"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -68,7 +62,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Desinta Anakusuma</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/5L1tzk9TF"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -77,9 +70,7 @@
                                             d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z" />
                                     </svg></a></td>
                         </tr>
-
                         <tr>
-
                             <td>Fajar Agro Wibowo</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/vcFilCuWN"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -89,7 +80,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Firsandy</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/36JOdvbO4"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -99,7 +89,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Hana Jelita Insani Pramo</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/BIHjvlIXn"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -109,7 +98,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Harianto</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/gp6lBJaJw"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -119,7 +107,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Hilda Astri Damayanti</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/9VWYL3xOU"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -193,7 +180,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Didik</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/bAJ5utbbk"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -203,7 +189,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Samsul Hidayat</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/lk9rq5mnx"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -213,7 +198,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Suwandi</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/9AmrWc20u"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -223,7 +207,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Toha</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/3Q7PRXnXS"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -233,7 +216,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Trisna Amrina</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/IuxZIusrR"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -243,7 +225,6 @@
                                     </svg></a></td>
                         </tr>
                         <tr>
-
                             <td>Wahyu Pria Purnama</td>
                             <td><a class="btn btn-primary" href="https://gofile.me/7hje9/yKmYAhERF"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -261,7 +242,6 @@
                                             d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z" />
                                     </svg></a></td>
                         </tr>
-
                     </tbody>
                 </table>
             </div>
