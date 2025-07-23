7750485a0c9bf9f3954114509d4fc4fddf4f3cc5
WahyuPriaPurnama <wahyupriapurnama@gmail.com>
2025-07-23 16:52:05 +0700

    Rename PDF component class to lowercase and create corresponding Blade view for PDF button

--- app/View/Components/Buttons/PDF.php
+++ app/View/Components/Buttons/PDF.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class PDF extends Component
+class Pdf extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class PDF extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.buttons.p-d-f');
+        return view('components.buttons.pdf');
     }
 }
--- resources/views/components/buttons/pdf.blade.php
+++ resources/views/components/buttons/pdf.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
-<a {{ $attributes->merge([
+    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+    <a {{ $attributes->merge([
         'class' => 'btn btn-danger',
     ]) }} target="_blank" data-bs-toggle="tooltip"
         data-bs-placement="top" data-bs-title="Export PDF"><i class="bi bi-filetype-pdf"></i>
