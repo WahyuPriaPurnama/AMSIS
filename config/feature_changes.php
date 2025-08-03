66c6bd3e66df91a2cb8396b63be8c84a91f8574a
WahyuPriaPurnama <wahyupriapurnama@gmail.com>
2025-08-03 20:20:45 +0700

    Update feature changes: refine PDF export description and add plant logo support

--- config/feature_changes.php
+++ config/feature_changes.php
@@ -3,6 +3,7 @@
 // config/feature_changes.php
 return [
     '29/07/2025' => 'Fitur ekspor PDF sudah mendukung logo plant dan foto',
-    '30/07/2025' => 'Data kendaraan sudah mendukung field tanggal service dan kilometer akhir, Terdapat copright pada footer PDF, nama file PDF mengikuti format data dan tanggal',
+    '30/07/2025' => 'Data kendaraan sudah mendukung field tanggal service dan kilometer akhir',
     '02/08/2025'=>'Tiap karyawan bisa login untuk update biodatanya, password bisa minta ke admin',
+    '03/08/2025'=>'logo masing-masing plant di tiap karyawan',
 ];
