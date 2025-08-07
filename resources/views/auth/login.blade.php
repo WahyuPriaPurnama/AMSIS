90f0e436e9225d737e771245790b987e186a72d1
WahyuPriaPurnama <wahyupriapurnama@gmail.com>
2025-08-07 16:59:28 +0700

    Enhance login form: add password visibility toggle and improve error feedback

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,14 +6,12 @@
             <div class="col-md-6">
                 @component('components.card')
                     @slot('header')
-                       <div class="text-center">Selamat Datang di AMS Information System</div>
+                        <div class="text-center">Selamat Datang di AMS Information System</div>
                     @endslot
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
                                     name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -25,23 +23,30 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password" required
-                                    autocomplete="current-password">
+                                <div class="input-group">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror" name="password" required
+                                        autocomplete="current-password">
+
+                                    <button type="button" class="input-group-text bg-white border-start-0"
+                                        onclick="togglePassword()" tabindex="-1" data-bs-toggle="tooltip"
+                                        data-bs-placement="right" title="Lihat Password">
+                                        
+                                        <i class="bi bi-eye" id="toggleIcon"></i>
+                                    </button>
+                                </div>
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
+                                    <span class="invalid-feedback d-block" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
                             </div>
                         </div>
-{{-- 
                         <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
@@ -53,21 +58,25 @@
                                     </label>
                                 </div>
                             </div>
-                        </div> --}}
-
+                        </div>
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" id="tooltip" class="btn btn-success" data-bs-toggle="tooltip" data-placement="top" title="klik untuk login">
+                                <button type="submit" id="tooltip" class="btn btn-success" data-bs-toggle="tooltip"
+                                    data-placement="top" title="klik untuk login">
                                     {{ __('Login') }}
                                 </button>
-                                
-                                <?php /*  @if (Route::has('password.request'))
+
+                                <a href="https://wa.me/6285745334330?text=Halo%20Admin,%20saya%20lupa%20password"
+                                    class="btn btn-link" target="_blank">
+                                    Lupa Password?
+                                </a>
+                                {{-- @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                @endif
-                                */
-                                ?>
+                                @endif --}}
+
+
                             </div>
                         </div>
                     </form>
@@ -75,4 +84,19 @@
             </div>
         </div>
     </div>
+    <script>
+        function togglePassword() {
+            const input = document.getElementById('password');
+            const icon = document.getElementById('toggleIcon');
+            if (input.type === 'password') {
+                input.type = 'text';
+                icon.classList.remove('bi-eye');
+                icon.classList.add('bi-eye-slash');
+            } else {
+                input.type = 'password';
+                icon.classList.remove('bi-eye-slash');
+                icon.classList.add('bi-eye');
+            }
+        }
+    </script>
 @endsection
