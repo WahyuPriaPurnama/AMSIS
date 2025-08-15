c3e77645ba3d2bc911feb1cc099029fb71afc4fb
WahyuPriaPurnama <wahyupriapurnama@gmail.com>
2025-08-15 10:32:51 +0700

    blue header

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -6,8 +6,10 @@ use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class UsersExport implements FromCollection, WithMapping, WithHeadings
+class UsersExport implements FromCollection, WithMapping, WithHeadings, WithStyles
 {
     /**
      * @return \Illuminate\Support\Collection
@@ -32,5 +34,20 @@ class UsersExport implements FromCollection, WithMapping, WithHeadings
     public function headings(): array
     {
         return ['No', 'Nama', 'Username', 'Plant'];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+
+        return [
+            1 => [
+                'font' => ['bold' => true, 'color' => ['rgb' => 'FFFFFF']],
+                'fill' => [
+                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                    'startColor' => ['rgb' => '3100ba']
+                ],
+                'alignment' => ['horizontal' => 'center']
+            ],
+        ];
     }
 }
